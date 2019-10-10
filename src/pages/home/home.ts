0393c619920a946517bbd3fad85eba152f475856
Khom Sovon <sovon.khom68@gmail.com>
2019-10-10 15:45:22 +0700

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Stripe } from '@ionic-native/stripe';
+import { Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+  public access_token = "";
+  public error = "";
 
-  constructor(public navCtrl: NavController) {
+  constructor(platform: Platform, private stripe: Stripe) {
 
+    platform.ready().then(() => {
+
+      this.stripe.setPublishableKey('pk_test_56b6630429c92062953adac23cd7420386e55ce60eb465cd0ad3ab226c09eb6a');
+
+      let card = {
+       number: '6200000000000005',
+       expMonth: 12,
+       expYear: 2020,
+       cvc: '220'
+      };
+
+      this.stripe.createCardToken(card)
+        .then(token => {
+          this.access_token = token.id;
+        })
+        .catch(error => {
+          this.error = error;
+        });
+
+    });
   }
-
 }
+
+
